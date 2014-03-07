464271ffba768c92e0a5241bc51524c5d745158b
Michal Mocny <mmocny@gmail.com>
2014-03-07 11:21:51 -0500

    geolocate on startup

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
+var _map = null;
+var _loc = null;
+var _markers = [];
+
+function addMarker(loc) {
+  markers.push(new google.maps.Marker({
+    map: _map,
+    position: loc
+  }));
+}
+
+function getLocationForAddress(address, callback) {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({
+    'address': address
+  }, function(results, status) {
+    if (status != google.maps.GeocoderStatus.OK) {
+      return console.error('geocoder.geocode failed with:', status);
+    }
+    console.log(results);
+
+    setTimeout(function() {
+      callback(results[0].geometry.location);
+    }, 0);
+  });
+}
+
+function getCurrentLocation(callback) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setTimeout(function() {
+      callback(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    }, 0);
+  }, function(err) {
+    console.error('geolocation.getCurrentPosition failed with:', err);
+  });
+}
+
 function initialize() {
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 2,
-    center: new google.maps.LatLng(0,0)
+  console.log(_loc);
+  _map = new google.maps.Map(document.getElementById('map-canvas'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 3,
+    center: _loc ? _loc : new google.maps.LatLng(0,0)
   });
 }
 
@@ -12,6 +51,14 @@ function loadMaps() {
   document.body.appendChild(script);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   loadMaps();
+  getCurrentLocation(function(loc) {
+    console.log(_map);
+    _loc = loc;
+    if (_map) {
+      _map.setCenter(loc);
+      addMarker(loc);
+    }
+  });
 });
